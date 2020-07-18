f82d0635a34d3df399813429e609b1a9bf59d925
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-18 21:18:19 +0600

    01-01 init

---
+++ 01-init-array/01_array_literal.js
@@ -0,0 +1,10 @@
+//array literal 
+const arr = []
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 5;
+console.log(arr.length) //arr.length gives the length of arr
+//output: 4
+console.log(arr)
+//[1,2,3,5]
