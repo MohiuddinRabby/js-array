54da7c997b254d9782b2808d0f92eef4705324bc
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-18 21:42:25 +0600

    01-01 array literal end

--- 01-init-array/01_array_literal.js
+++ 01-init-array/01_array_literal.js
@@ -8,3 +8,16 @@ console.log(arr.length) //arr.length gives the length of arr
 //output: 4
 console.log(arr)
 //[1,2,3,5]
+arr[10] = 11;
+console.log(arr)
+//output: [ 1, 2, 3, 5, <6 empty items>, 11 ]
+
+const newArr = ['john', 'doe', 'steve', 'mike']
+//init newArr with value - 'john','doe','steve','mike'
+console.log(newArr)
+//[ 'john', 'doe', 'steve', 'mike' ]
+newArr[4] = 'smith'
+console.log(newArr)
+//[ 'john', 'doe', 'steve', 'mike', 'smith' ]
+newArr[newArr.length] = 'jonson';
+console.log(newArr)
