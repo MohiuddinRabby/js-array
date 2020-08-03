60a7a2c8f5fd36f6572a20622581e46506bf12e8
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-08-03 20:43:46 +0600

    filter map example

---
+++ filter_map.js
@@ -0,0 +1,16 @@
+const items = [
+  { id: 1, name: "item1", category: "electronics" },
+  { id: 2, name: "item2", category: "tv" },
+  { id: 3, name: "item3", category: "electronics" },
+  { id: 4, name: "item4", category: "gadget" },
+  { id: 6, name: "item5", category: "gadget" },
+  { id: 7, name: "item6", category: "gadget" },
+  { id: 8, name: "item7", category: "mobile" },
+  { id: 9, name: "item8", category: "electronics" },
+  { id: 10, name: "item9", category: "mobile" },
+  { id: 11, name: "item10", category: "tv" },
+];
+const allData = items.map((item) => item);
+// console.log(allData);
+const filteredData = items.filter((cat) => cat.category === "electronics");
+console.log(filteredData);
